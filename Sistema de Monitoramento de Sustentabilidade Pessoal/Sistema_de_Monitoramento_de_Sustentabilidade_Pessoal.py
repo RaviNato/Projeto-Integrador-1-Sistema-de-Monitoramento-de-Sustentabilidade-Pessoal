3c60e20da97c322d0788e686e09947621fef4b26
Caio Lucena <caio9lucena@gmail.com>
2025-03-30 17:56:24 -0300

    Realizado processamento das entradas, e adicionado saída

--- Sistema de Monitoramento de Sustentabilidade Pessoal/Sistema_de_Monitoramento_de_Sustentabilidade_Pessoal.py
+++ Sistema de Monitoramento de Sustentabilidade Pessoal/Sistema_de_Monitoramento_de_Sustentabilidade_Pessoal.py
@@ -1,7 +1,8 @@
 ﻿print("################        PROGRAMA DE MONITORAMENTO DE SUSTENTABILIDADE PESSOAL     #######################")
 
+
 nome = input("Qual seu nome? ")
-data = input(f"Olá, {nome}, qual a data do dia referente (00/00/0000)? ")
+data = input(f"Olá, {nome}, qual a data do dia referente (00-00-0000)? ")
 
 digitoBem = False
 while not digitoBem:
@@ -9,7 +10,7 @@ while not digitoBem:
         litros = float(input("Quantos litros de água você consumiu hoje (Aprox. em litros)? "))
         kwh = float(input("Quantos kWh de energia elétrica você consumiu hoje? "))
         kg = float(input("Quantos kg de resíduos não recicláveis você gerou hoje? "))
-        porcentagem = float(input("Qual a porcentagem de resíduos reciclados no total? "))
+        porcentagem = float((input("Qual a porcentagem de resíduos reciclados no total? ")).replace("%", "").strip())
         print(
         " [1] Transporte público (ônibus, metrô, trem)\n",\
         "[2] Bicicleta\n",\
@@ -17,28 +18,69 @@ while not digitoBem:
         "[4] Carro (combustível fósseis)\n",\
         "[5] Carro elétrico\n",\
         "[6] Carona compartilhada")
-        transporte = float(input("Qual o meio de transporte você usou hoje? "))
+        transporte = int(input("Qual o meio de transporte você usou hoje? "))
     except ValueError:
-        print(f"{nome}, o valor deverá ser um número!")
+        print(f"{nome}, o valor deverá ser um número! Tente novamente.")
     else:
         if litros < 0 or kwh < 0 or kg < 0 or porcentagem < 0:
-            print(f"{nome}, o valor deverá ser um valor positivo!")
+            print(f"{nome}, o valor deverá ser um valor positivo! Tente novamente.")
+        elif porcentagem > 100:
+            print(f"{nome}, a porcentagem de resíduos reciclados não deve ultrapassar 100%! Tente novamente.")
         elif transporte > 6 or transporte < 1:
-            print(f"{nome}, não há correspondência para esse transporte!")
+            print(f"{nome}, não há correspondência para esse transporte! Tente novamente.")
         else:
             digitoBem = True
 
 
 
-    
+
+consumAgua = None
+if litros < 150:
+    consumAgua = "Alta Sustentabilidade"
+elif litros > 151 and litros < 201:
+    consumAgua = "Moderada Sustentabilidade"
+else:
+    consumAgua = "Baixa Sustentabilidade"
+
+consumEnergia = None
+if kwh < 5:
+    consumEnergia = "Alta Sustentabilidade"
+elif kwh > 4 and kwh < 11:
+    consumEnergia = "Moderada Sustentabilidade"
+else:
+    consumEnergia = "Baixa Sustentabilidade"
+
+
+consumLixo = None
+if porcentagem > 50:
+    consumLixo = "Alta Sustentabilidade"
+elif porcentagem > 19 and porcentagem < 51:
+    consumLixo = "Moderada Sustentabilidade"
+else:
+    consumLixo = "Baixa Sustentabilidade"
+
+consumTransporte = None
+if transporte == 1 or transporte == 2 or transporte == 3 or transporte == 5:
+    consumTransporte = "Alta Sustentabilidade"
+elif transporte == 4:
+    consumTransporte = "Baixa Sustentabilidade"
+else:
+    consumTransporte = "Moderada Sustentabilidade"
 
 
 
 
 
 
-
-
+print(
+    "\n",\
+    f"#########      Chegamos ao fim, {nome}. Aqui está o resultado de suas classificações!       #########\n",\
+    "\n",\
+    f"  Consumo de água:                        {consumAgua}\n",\
+    f"  Geração de Resíduos Não Recicláveis:    {consumLixo}\n",\
+    f"  Consumo de Energia Elétrica:            {consumEnergia}\n",\
+    f"  Uso de Transporte:                      {consumTransporte}\n",\
+    "")
 
 
 
